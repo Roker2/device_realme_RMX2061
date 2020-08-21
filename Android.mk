1106fb4f5fa7e8c0f064a06f537e6dae7908388f
Roker2 <dimaminko2000@gmail.com>
2020-08-21 21:11:58 +0300

    RMX2061: Add EGL Symlink

--- Android.mk
+++ Android.mk
@@ -1,5 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RMX2061)
-include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
+
+EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
+$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
